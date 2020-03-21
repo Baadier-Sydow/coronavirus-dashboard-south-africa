e157d6432d8234cefaf66b437a4a4a0e3f2985eb
Baadier Sydow <bsydow@gmail.com>
2020-03-21 19:26:13 +0200

    Updated endpoints for testing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,19 @@
 export const environment = {
   production: true,
   api: {
-    local: 'https://covid-za-api.herokuapp.com/',
-    international: 'https://api.covid19api.com/',
-    origin: 'https://raw.githubusercontent.com/dsfsi/covid19za/master/data/'
+    local: {
+      url: 'https://covid-za-api.herokuapp.com/',
+      confirmed: 'cases/confirmed',
+    },
+    international: {
+      url: 'https://api.covid19api.com/'
+    },
+    origin: {
+      url: 'https://raw.githubusercontent.com/dsfsi/covid19za/master/data/',
+      confirmed: 'covid19za_timeline_confirmed.csv',
+      transmission: 'covid19za_timeline_transmission_type.csv',
+      testing: 'covid19za_timeline_testing.csv',
+      deaths: 'covid19za_timeline_deaths.csv'
+    }
   }
 };
