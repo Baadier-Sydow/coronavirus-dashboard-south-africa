a4546dccee967c3c143f80d8ae4908da90646e0a
Baadier Sydow <bsydow@gmail.com>
2020-03-22 22:08:42 +0200

    Provide citation for local data

--- src/app/data/data.page.ts
+++ src/app/data/data.page.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DataPage implements OnInit {
 
   title = 'Data Sources';
+  localDataCitation = '@dataset{marivate_vukosi_2020_3723347, author = {Marivate, Vukosi and de Waal, Alta and Combrink, Herkulaas and Lebogo, Ofentswe and Moodley, Shivan and Mtsweni, Nompumelelo and Rikhotso, Vuthlari}, title = {{Coronavirus disease (COVID-19) case data - South Africa}}, month = mar, year = 2020, publisher = {Zenodo}, doi = {10.5281/zenodo.3723347}, url = {https://doi.org/10.5281/zenodo.3723347} }';
   constructor() { }
 
   ngOnInit() {
